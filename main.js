2c0c23aba9b286f0092774873dd7fe3e2dd1125a
dam450 <68165752+dam450@users.noreply.github.com>
2021-12-14 13:56:26 -0300

    add random food and snake size
    Signed-off-by: dam450 <68165752+dam450@users.noreply.github.com>

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ let context = canvas.getContext('2d')
 let box = 32
 let snake = []
 
+canvas.setAttribute('height', 512)
+canvas.setAttribute('width', 512)
+
 snake[0] = {
   x: 8 * box,
   y: 8 * box
@@ -59,7 +62,12 @@ function startGame() {
   if (direction == 'up') snakeY -= box
   if (direction == 'down') snakeY += box
 
-  snake.pop()
+  if (snakeX != food.x || snakeY != food.y) {
+    snake.pop()
+  } else {
+    food.x = Math.floor(Math.random() * 15 + 1) * box
+    food.y = Math.floor(Math.random() * 15 + 1) * box
+  }
 
   let newHead = {
     x: snakeX,
